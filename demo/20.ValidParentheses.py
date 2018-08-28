defc5fc2f83d055387154e403c189f57613f3425
symb <echolan@126.com>
2018-08-29 01:05:41 +0800

    20.有效的括号

---
+++ demo/20.ValidParentheses.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+YES, WE CAN!
+@Time        : 2018/8/29 0:35
+@Author      : 兰兴宝 echolan@126.com
+@File        : 20.ValidParentheses.py
+@Site        : 
+@Project     : LeetCodeSymb
+@Note        : [describe how to use it]
+"""
+# Enjoy Your Code
+
+
+class Solution:
+    """
+    判断一串由 '(', ')', '{', '}', '[' 和 ']' 组成的字符串是否有效。
+
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+    An input string is valid if:
+
+        1、Open brackets must be closed by the same type of brackets.
+        2、Open brackets must be closed in the correct order.
+    Note that an empty string is also considered valid.
+
+    Example 1:
+        Input: "()"
+        Output: true
+    Example 2:
+        Input: "()[]{}"
+        Output: true
+    Example 3:
+        Input: "(]"
+        Output: false
+    Example 4:
+        Input: "([)]"
+        Output: false
+    Example 5:
+        Input: "{[]}"
+        Output: true
+    """
+    def symb(self, s):
+        """
+        :type s:  str
+        :rtype: bool
+        """
+        right = ''
+        d = {
+            '(': ')',
+            '{': '}',
+            '[': ']',
+        }
+        for x in s:
+            if x in d:
+                right = d[x] + right
+            else:
+                if x == right[:1]:
+                    right = right[1:]
+                else:
+                    return False
+        return False if right else True
+
