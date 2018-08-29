1e6fd87c4828498d01b7845d37af44ec3ffb805b
symb <echolan@126.com>
2018-08-29 08:54:19 +0800

    20.有效的括号 补充高赞实现

--- demo/20.ValidParentheses.py
+++ demo/20.ValidParentheses.py
@@ -61,3 +61,16 @@ class Solution:
                     return False
         return False if right else True
 
+    # 高赞实现
+    def isValid(self, s):
+        stack = []
+        dict = {"]": "[", "}": "{", ")": "("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []
