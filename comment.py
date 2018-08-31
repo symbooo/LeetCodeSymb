bb4fb58b4a40620f6df6f034a6c38c8ffb16a00d
symb <echolan@126.com>
2018-08-31 17:41:36 +0800

    公共模块

---
+++ comment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+YES, WE CAN!
+@Time        : 2018/8/31 17:40
+@Author      : 兰兴宝 echolan@126.com
+@File        : comment.py
+@Site        : 
+@Project     : LeetCodeSymb
+@Note        : [describe how to use it]
+"""
+# Enjoy Your Code
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
