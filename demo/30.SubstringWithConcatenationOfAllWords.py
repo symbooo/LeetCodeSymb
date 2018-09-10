dd5e0ed91e3319c27553dee23a0ccaf5594eb749
symb <echolan@126.com>
2018-09-10 23:57:46 +0800

    30.字符串中找字串，待解

---
+++ demo/30.SubstringWithConcatenationOfAllWords.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+YES, WE CAN!
+@Time        : 2018/9/10 23:54
+@Author      : 兰兴宝 echolan@126.com
+@File        : 30.SubstringWithConcatenationOfAllWords.py
+@Site        : 
+@Project     : LeetCodeSymb
+@Note        : [describe how to use it]
+"""
+# Enjoy Your Code
+
+class Solution:
+    """
+
+
+    """
+    def symb(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
