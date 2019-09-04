df15e3c91928e87d4bd9e0f9adde28c675879055
symb <echolan@126.com>
2019-09-05 00:47:27 +0800

    35.查找插入位置

---
+++ demo/Array/35.SearchInsertPosition.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+YES, WE CAN!
+@Time        : 2019/9/5 0:11
+@Author      : echolan@126.com
+@File        : 35.SearchInsertPosition.py
+@Site        : 
+@Project     : LeetCodeSymb
+@Note        : [describe how to use it]
+"""
+# Enjoy Your Code
+from typing import List
+
+
+class Solution:
+    """
+Given a sorted array and a target value, return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+    Input: [1,3,5,6], 5
+    Output: 2
+
+Example 2:
+    Input: [1,3,5,6], 2
+    Output: 1
+
+Example 3:
+    Input: [1,3,5,6], 7
+    Output: 4
+
+Example 4:
+    Input: [1,3,5,6], 0
+    Output: 0
+
+给定一个有序数组和一个目标值，返回该值在数组中的节点值或者返回该插入的位置，默认数组元素不重复
+    """
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i = 0
+        for i, v in enumerate(nums):
+            if v >= target:
+                return i
+        else:
+            return i
+
+# most vote
+def searchInsert(self, nums, target):
+    l , r = 0, len(nums)-1
+    while l <= r:
+        mid=(l+r)/2
+        if nums[mid]== target:
+            return mid
+        if nums[mid] < target:
+            l = mid+1
+        else:
+            r = mid-1
+    return l
+
+
